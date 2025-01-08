cd0e4ae5ae9e7fbb642bd55c750bd57e3e4b949e
Trumcode124 <atvn15@gmail.com>
2025-01-08 11:01:09 +0700

    update bai 3

--- Bai3/handler.js
+++ Bai3/handler.js
@@ -32,6 +32,24 @@ function showHandler(Dolphins, Koalas){
     console.log("It's a draw");
     }
 }
+
+function showHandler_1(Dolphins, Koalas){
+    const avg_Dolphins = Dolphins.AvgHandel();
+    const avg_Koalas = Koalas.AvgHandel();
+
+    console.log(`Diem trung binh cua Dolphins: ${avg_Dolphins}, diem trung binh cua Koalas: ${avg_Koalas}`);
+
+    var check_1 = avg_Dolphins >= 100;
+    var check_2 = avg_Koalas >= 100;
+
+    if (avg_Dolphins > avg_Koalas && check_1) {
+    console.log("Dolphins win");
+    } else if (avg_Dolphins < avg_Koalas && check_2) {
+    console.log("Koalas win");
+    } else if(check_1 || check_2){
+    console.log("It's a draw");
+    }
+}
 // Data 1
 const Dolphins = new Team("Dolphins", [96, 108, 89]);
 const Koalas = new Team("Koalas", [88, 91, 110]);
@@ -40,11 +58,11 @@ showHandler(Dolphins,Koalas);
 // Data 1
 const Dolphins_1 = new Team("Dolphins", [97, 112, 101]);
 const Koalas_1 = new Team("Koalas", [109, 95, 123]);
-showHandler(Dolphins_1,Koalas_1);
+showHandler_1(Dolphins_1,Koalas_1);
 
 // Data 1
 const Dolphins_2 = new Team("Dolphins", [97, 112, 101]);
 const Koalas_2 = new Team("Koalas", [109, 95, 106]);
-showHandler(Dolphins_2,Koalas_2);
+showHandler_1(Dolphins_2,Koalas_2);
 
 
