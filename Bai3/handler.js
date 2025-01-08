e4a1f19b4e960f7736b9ea559d2b25ae29bd2496
H82M19\Student <Student>
2025-01-08 10:42:49 +0700

    update bai 3: There are two gymnastics teams, Dolphins and Koalas

--- Bai3/handler.js
+++ Bai3/handler.js
@@ -18,21 +18,33 @@ class Team {
   }
 }
 
+function showHandler(Dolphins, Koalas){
+    const avg_Dolphins = Dolphins.AvgHandel();
+    const avg_Koalas = Koalas.AvgHandel();
+
+    console.log(`Diem trung binh cua Dolphins: ${avg_Dolphins}, diem trung binh cua Koalas: ${avg_Koalas}`);
+
+    if (avg_Dolphins > avg_Koalas) {
+    console.log("Dolphins win");
+    } else if (avg_Dolphins < avg_Koalas) {
+    console.log("Koalas win");
+    } else {
+    console.log("It's a draw");
+    }
+}
+// Data 1
 const Dolphins = new Team("Dolphins", [96, 108, 89]);
 const Koalas = new Team("Koalas", [88, 91, 110]);
+showHandler(Dolphins,Koalas);
+
+// Data 1
+const Dolphins_1 = new Team("Dolphins", [97, 112, 101]);
+const Koalas_1 = new Team("Koalas", [109, 95, 123]);
+showHandler(Dolphins_1,Koalas_1);
+
+// Data 1
+const Dolphins_2 = new Team("Dolphins", [97, 112, 101]);
+const Koalas_2 = new Team("Koalas", [109, 95, 106]);
+showHandler(Dolphins_2,Koalas_2);
 
 
-const avg_Dolphins = Dolphins.AvgHandel();
-const avg_Koalas = Koalas.AvgHandel();
-
-
-console.log(`Diem trung binh cua Dolphins: ${avg_Dolphins}, diem trung binh cua Koalas: ${avg_Koalas}`);
-
-
-if (avg_Dolphins > avg_Koalas) {
-  console.log("Dolphins win");
-} else if (avg_Dolphins < avg_Koalas) {
-  console.log("Koalas win");
-} else {
-  console.log("It's a draw");
-}
